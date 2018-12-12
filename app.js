e5892ab38f0a641a877fcc8319a55c72043e9118
iamakshatjain <32021987+iamakshatjain@users.noreply.github.com>
2018-12-12 23:09:14 +0530

    add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app=express();
 var bodyParser=require("body-parser");
+var mongoose=require("mongoose");
+//connecting mongoose
+mongoose.connect("mongodb://localhost:/yelpcamp",{ useNewUrlParser: true });
 
 app.set("view engine","ejs");
 app.use(bodyParser.urlencoded({extended:true}));
@@ -8,61 +11,67 @@ app.get("/",function(req,res){
 	res.render("landing");
 });
 
-var camps = [
-		{
-			location:"Tsomoriri Camp",
-			image:"https://cincinnatiusa.com/sites/default/files/styles/article_full/public/attractionphotos/Winton%20Woods%20Campground.JPG?itok=xqTUl4YJ"
-		},
-		{
-			location:"Jaipur campgrounds",
-			image:"http://www.tourideas-usa.com/home/infos/fotos/super04.jpg"
-		},
-		{
-			location:"New Delhi campgrounds",
-			image:"https://www.tripsavvy.com/thmb/FlJvHzJ3A8pTHcbHS46zYDdHhmk=/870x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/102145689-56a0e43d5f9b58eba4b4e578.jpg"
-		},
-		{
-			location:"Camp Exotica",
-			image:"https://cdn.pixabay.com/photo/2016/02/18/22/16/tent-1208201__340.jpg"
-		},
-		{
-			location:"Rishikesh Valley",
-			image:"https://cdn.pixabay.com/photo/2016/01/19/16/48/teepee-1149402__340.jpg"
-		},
-		{
-			location:"Dehradun",
-			image:"https://cdn.pixabay.com/photo/2016/11/21/15/14/camping-1845906__340.jpg"
-		},
-		{
-			location:"Manali grounds",
-			image:"https://cdn.pixabay.com/photo/2015/10/12/14/57/campfire-984020__340.jpg"
-		},
-		{
-			location:"Shimla shelters",
-			image:"https://cdn.pixabay.com/photo/2016/11/22/23/08/adventure-1851092__340.jpg"
-		},
-		{
-			location:"Nameri Eco Camp",
-			image:"https://cdn.pixabay.com/photo/2014/11/27/18/36/tent-548022__340.jpg"
-		},
-
-	];
-
-app.get("/campgrounds",function(req,res){
-	
-	res.render("campgrounds",{camps:camps})
+//schema for campgrounds
+var campgroundSchema = new mongoose.Schema({
+	location:String,
+	image:String,
+	desc:String
 });
 
+//creating model for campgrounds
+//INDEX-to display all the available campgrounds
+var campground = mongoose.model("campground",campgroundSchema);//this creates a collection named as campgrounds
+
+app.get("/campgrounds",function(req,res){
+
+	campground.find({},function(err,camps){
+		if(err){
+			console.log("error from line#78")
+			console.log(err);
+		}
+		else
+			res.render("index",{camps:camps});
+	})
+});
+
+//NEW-display form for new campground
+app.get("/campgrounds/new",function(req,res){//the form for adding a new camp ground
+	res.render("new.ejs");//this sends a post request to the /campgrounds
+});
+
+//CREATE-to create new campground into the db
 app.post("/campgrounds",function(req,res){//to add to the campgrounds
-	var camp={location:"",image:""}
+	var camp={location:"",image:"",desc:""}
 	camp.location=req.body.location;
 	camp.image=req.body.image;
-	camps.push(camp);
+	camp.desc=req.body.desc;
+	// camps.push(camp);
+	campground.create(camp,function(err,camp){
+		if(err)
+			console.log(err);
+		else
+		{
+			console.log("new campground added");
+			console.log(camp);
+		}
+	});
 	res.redirect("/campgrounds");
 });
 
-app.get("/campgrounds/new",function(req,res){//the form for adding a new camp ground
-	res.render("new.ejs");
+//SHOW-to show the details of particular campsites
+app.get("/campgrounds/:id",function(req,res){
+	var id=req.params.id;
+	campground.findById(id,function(err,camp){//important method findById
+		if(err)
+		{
+			console.log("there is an error in line#66");
+			console.log(err);
+		}
+		else
+		{
+			res.render("show",{camp:camp});
+		}
+	});
 });
 
 app.listen(process.env.PORT || 3000,process.env.IP,function(){
